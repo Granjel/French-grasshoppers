e2f1553ed69049f48550e6c3b0579a0dfa177312
Rodrigo R. Granjel <granjel@gmail.com>
2019-01-07 22:58:18 +0100

    commit

--- Code/metrics.R
+++ Code/metrics.R
@@ -8,8 +8,8 @@ gamma4 <- read.table("Results/output_glinternet/gamma4.txt", header = TRUE, sep 
 gamma5 <- read.table("Results/output_glinternet/gamma5.txt", header = TRUE, sep = "\t") * (-1)
 gamma6 <- read.table("Results/output_glinternet/gamma6.txt", header = TRUE, sep = "\t") * (-1)
 
-#intrinsic --- from 0.01 to 1 by 0.05 (strength)
-intr_strength_seq <- seq(from = 0.01, to = 1, by = 0.05) #modif
+#intrinsic --- from 0.01 to 1 by 0.01 (strength)
+intr_strength_seq <- seq(from = 0.01, to = 1, by = 0.01) #modif
 intr_strength <- list()
 xx <- matrix(nrow = 22, ncol = 2)
 colnames(xx) <- c("intr", "strength")
@@ -23,49 +23,30 @@ for (i in 1:length(intr_strength_seq)){
   intr_strength[[i]] <- xx
 }
 
-#intrinsic --- from 1 to 24 by 1 (abundance)
-intr_abundance_seq <- seq(from = 1, to = 24, by = 1) #modif
-intr_abundance <- list()
-xx <- matrix(nrow = 22, ncol = 2)
-colnames(xx) <- c("intr", "abundance")
-rownames(xx) <- c("ACHMIL", "ARRELA", "BROERE", "CENJAC", "CONARV", "CREPIS", "DACGLO", "DAUCAR", "ELYREP", "ERYNGE", "FESRUB",
-                  "GALVER", "LEUVUL", "ONOREP", "PICECH", "PLALAN", "POAANG", "RANACR", "RUMACE", "SALPRA", "TAROFF", "TRIPRA")
-for (i in 1:length(intr_abundance_seq)){
-  for (j in 1:nrow(xx)){
-    xx[j, 1] <- lambda[j,] + rowSums(sigma[j,]) * intr_abundance_seq[i]
-    xx[j, 2] <- intr_abundance_seq[i]
-  }
-  intr_abundance[[i]] <- xx
-}
-
-#alphas --- from 0.01 to 1 by 0.05 (strength)
+#alphas --- from 0.01 to 1 by 0.01 (strength)
 gamma <- gamma1 + gamma2 + gamma3 + gamma4 + gamma5 + gamma6
-comp_strength_seq <- seq(from = 0.01, to = 1, by = 0.05) #modif
+comp_strength_seq <- seq(from = 0.01, to = 1, by = 0.01) #modif
 comp_strength <- list()
 for (i in 1:length(comp_strength_seq)){
   yy <- alpha + gamma * comp_strength_seq[i]
   comp_strength[[i]] <- yy
 }
 
-#alphas --- from 1 to 24 by 1 (abundance)
-gamma <- gamma1 + gamma2 + gamma3 + gamma4 +  gamma6 #include gamma5
-comp_abundance_seq <- seq(from = 1, to = 24, by = 1) #modif
-comp_abundance <- list()
-for (i in 1:length(comp_abundance_seq)){
-  yy <- alpha + gamma * comp_abundance_seq[i]
-  comp_abundance[[i]] <- yy
-}
+a <- intr_strength[[100]]
+b <- comp_strength[[100]]
 
 #rm
 rm(i, j, xx, yy)
 
+#strength of the links --- OK
+#variability --- gamma and sigma
+#random network modifications --- network metrics --- complex networks: "muzviz"
+##remove a increasing percentage of links + massively replicate each try
 
-#####
+#calculate structural approach's outputs
+#plot
 
-#install.packages("bipartite")
-library(bipartite)
-nestedness(sigma) #¿?
-
+#tell a story
 
 
 
