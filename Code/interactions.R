c8c2bcc2f4e8d3f0dab8983501943e877742e5c6
Granjel <granjel@gmail.com>
2019-01-18 11:08:11 +0100

    interactions still a mess but better

--- Code/interactions.R
+++ Code/interactions.R
@@ -43,14 +43,122 @@ wine(deletions25[[4]], nreps = 10)$'win'
 wine(deletions25[[4]], nreps = 10)$wine
 
 
-
-
 V.ratio(sigma) #Calculates the variance-ratio as suggested by Schluter (1984)
 computeModules(sigma)
 nested
 degreedistr(sigma)
 
 
+###################################################################################
+###################################################################################
+
+
+#seleccionar matrices variando la conectancia
+
+sigma <- as.matrix(read.table("Results/output_glinternet/sigma.txt", header = TRUE, sep = "\t") * (-1))
+
+value <- 0 #zero or NA?
+nsigma <- as.numeric(nrow(sigma)*ncol(sigma))
+nmiss <- nsigma * 0.25
+reps <- 20
+
+deletions25 <- list()
+deletions50 <- list()
+deletions75 <- list()
+for (i in 1:reps){
+  sigma25 <- sigma
+  sigma50 <- sigma
+  sigma75 <- sigma
+  
+  sigma25[sample(nsigma, nmiss)] <- value
+  sigma50[sample(nsigma, nmiss*2)] <- value
+  sigma75[sample(nsigma, nmiss*3)] <- value
+  
+  deletions25[[i]] <- sigma25
+  deletions50[[i]] <- sigma50
+  deletions75[[i]] <- sigma75
+}
+
+rm(i, nsigma, nmiss, sigma25, sigma50, sigma75)
+
+##
+sigma <- as.matrix(read.table("Results/output_glinternet/sigma.txt", header = TRUE, sep = "\t") * (-1))
+
+conectance_matrices <- list()
+modifs_conectance <- c(0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9) #conectance values --- modify as needed
+reps <- 10 #reps for each conectance value --- modify as needed
+nsigma <- as.numeric(nrow(sigma)*ncol(sigma))
+value <- 0 #zero or NA?
+
+for (i in 1:length(modifs_conectance)){
+  conectance_matrices[[i]] <- list()
+  ndeletions <- 1 - modifs_conectance[i]
+  for (j in 1:reps){
+    modify_sigma <- sigma
+    modify_sigma[sample(nsigma, ndeletions)] <- value
+    conectance_matrices[[i]][[j]] <- modify_sigma
+  }
+}
+
+
+conectance_matrices[[1]][[1]]
+
+
+
+
+
+
+
+sigma <- as.matrix(read.table("Results/output_glinternet/sigma.txt", header = TRUE, sep = "\t") * (-1))
+
+conectance_values <- c(0.2, 0.4, 0.6, 0.8) #modif if needed
+conectance_matrices <- list() #selected matrices
+order_matrices <- list() #order in which these matrices showed up and were selected
+conectance_residuals <- list() #matrices not selected
+order_residuals <- list() ##order in which these matrices showed up and weren't selected
+
+reps <- 10 #reps for each conectance value --- modify as needed
+nsigma <- as.numeric(nrow(sigma)*ncol(sigma))
+value <- 0 #zero or NA?
+
+for (i in 1:conectance_values){
+  
+  conectance_matrices[[i]] <- list() 
+  order_matrices[[i]] <- list()
+  conectance_residuals[[i]] <- list()
+  order_residuals[[i]] <- list()
+  
+  ndeletions <- 1 - modifs_conectance[i]
+  
+  modify_sigma <- sigma
+  modify_sigma[sample(nsigma, ndeletions)] <- value
+  conectance_matrices[[i]][[j]] <- modify_sigma  
+  
+  
+  
+  
+}
+
+
+
+
+
+try <- list(list(list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list())),
+            list(list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list())),
+            list(list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list())),
+            list(list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list()), list(list(), list(), list(), list())))
+
+
+for (i in 1:4){
+  for (j in 1:4){
+    for (k in 1:4){
+      for (q in 1:10){
+        try[[i]][[j]][[k]][[q]] <- runif(1, 0, 1)
+      }
+    }
+  }
+}
+try[[1]][[1]][[1]][[5]]
 
 
 
@@ -60,31 +168,56 @@ degreedistr(sigma)
 
 
 
+sigma <- as.matrix(read.table("Results/output_glinternet/sigma.txt", header = TRUE, sep = "\t") * (-1))
+
+nsigma <- as.numeric(nrow(sigma)*ncol(sigma))
+value <- 0 #zero or NA?
+ndeletions <- nsigma * 0.8
+
+modify_sigma <- sigma
+modify_sigma[sample(nsigma, ndeletions)] <- value
+ 
+
+library(bipartite)
+
+nested(sigma, method = "weighted NODF", rescale = FALSE, normalised = FALSE)
+nested(modify_sigma, method = "weighted NODF", rescale = FALSE, normalised = FALSE)
 
 
 
+#check the possible range and distribution of nestedness
+percentage <- seq(from = 0.1, to = 0.9, by = 0.05)
+reps <- 1000
 
+value <- 0 #zero or NA?
 
+conectance <- NULL
+nest <- NULL
 
+for (i in 1:length(percentage)){
+  for (j in 1:reps){
+    
+    modify_sigma <- sigma
+    modify_sigma[sample(nsigma, ndeletions)] <- value
+    
+    conectance <- c(conectance, (1 - percentage[i]))
+    nest <- c(nest, nested(modify_sigma, method = "weighted NODF", rescale = FALSE, normalised = FALSE))
+    #modularity
+    
+  }
+}
 
+df <- data.frame(conectance, nest) #add modularity
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+boxplot(df$nest ~ df$conectance)
+range(df$nest)
+mean(df$nest)
+quantile(df$nest)
+getmode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+getmode(df$nest)
 
 
 
